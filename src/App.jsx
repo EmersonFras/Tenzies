db2449ed3ba2a52d396a747bc8403a2127ff1e0a
emersonfras <emersonfrasure@gmail.com>
2025-01-19 16:35:48 -0500

    Adds 10 random dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { useState } from 'react'
+import Die from './components/Die'
+
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+
+  const diceElement = dice.map((value, index) => (<Die value={value} key={index} />))
+
   return (
     <main>
+      <div className="dices">
+        {diceElement}
+      </div>
     </main>
   )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Die({ value }) {
+    return (
+        <button className="die">{value}</button>
+    )
+}
+
+export default Die
