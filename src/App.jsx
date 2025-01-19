b7ecce31fd51dde7bd54ee26fdd296ac35454db1
emersonfras <emersonfrasure@gmail.com>
2025-01-19 17:09:45 -0500

    Styling for held dice, temp hold die function
    Must finish hold die function, currently just console log. Green styling for a die that is held. Updates die state to object with value and whether die is held.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,35 @@ import Die from './components/Die'
 
 function App() {
 
-  const [dice, setDice] = useState(allNewDice())
+    const [dice, setDice] = useState(allNewDice())
 
-  function allNewDice() {
-    return new Array(10)
-      .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
-  }
+    function allNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({ 
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false
+            }))
+    }
 
-  function rollDice() {
-    setDice(allNewDice())
-  }
+    function rollDice() {
+        setDice(allNewDice())
+    }
 
-  const diceElement = dice.map((value, index) => (<Die value={value} key={index} />))
+    function holdDie(id) {
+        console.log(id)
+    }
 
-  return (
-    <main>
-      <div className="dices">
-        {diceElement}
-      </div>
-      <button className="dice-roll" onClick={rollDice}>Roll</button>
-    </main>
-  )
+    const diceElement = dice.map((data, index) => (<Die holdDie={holdDie} value={data.value} id={index} key={index} isHeld={data.isHeld} />))
+
+    return (
+        <main>
+            <div className="dices">
+                {diceElement}
+            </div>
+            <button className="dice-roll" onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
 
 export default App
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Die({ value }) {
+function Die({ value, isHeld, holdDie, id }) {
+
+    const styles = {
+        backgroundColor: isHeld ? "#59E391" : undefined
+    }
+
     return (
-        <button className="die">{value}</button>
+        <button onClick={() => holdDie(id)} style={styles} className="die">{value}</button>
     )
 }
 
